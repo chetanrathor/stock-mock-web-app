eb9ce6177e7b46316c6c81aa8c3c7bdffafd0444
chetan <chetan@gmail.com>
2023-10-21 16:35:45 +0530

    complete

--- src/store/app-store.ts
+++ src/store/app-store.ts
@@ -1,17 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { stockReducer } from '../features/stock/state/stock-slice'
-import { useDispatch } from 'react-redux'
-import { getStcokApi } from '../features/stock/api'
 
 const store = configureStore({
     reducer: {
-        [getStcokApi.reducerPath]: getStcokApi.reducer
-    },
-    middleware: (getDefaultMiddleware) => {
-        return getDefaultMiddleware().concat(getStcokApi.middleware)
+        stock: stockReducer
     }
 })
 
 export type RootState = ReturnType<typeof store.getState>
-export type useAppDispatch = typeof store.dispatch
 export default store
